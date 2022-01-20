5fc7790c14cfcd3399268a2a8374e3c6108e6f2e
Eder Lima <devilsouls666@gmail.com>
2022-01-20 01:29:00 -0300

    feat: add models and update schema

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from events.models import Speaker, SpeakerSocialMedia
+from events.models import (
+    Stage,
+    Speaker,
+    Keynote,
+    KeynoteSpeaker,
+    SpeakerSocialMedia,
+)
 
-# Register your models here.
+
+admin.site.register(Stage)
 admin.site.register(Speaker)
+admin.site.register(Keynote)
+admin.site.register(KeynoteSpeaker)
 admin.site.register(SpeakerSocialMedia)
--- events/models.py
+++ events/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 from uuid import uuid4
 
+class Stage(models.Model):
+    id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
+    name = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Keynote(models.Model):
+    id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
+    title = models.CharField(max_length=255)
+    schedule = models.CharField(max_length=64)
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
 class Speaker(models.Model):
     id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
@@ -9,6 +30,7 @@ class Speaker(models.Model):
     company = models.CharField(max_length=255)
     bio = models.TextField()
     image_url = models.URLField(max_length=512)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -16,16 +38,17 @@ class Speaker(models.Model):
         return self.name
 
 
-# class SpeakerTalk(models.Model):
-#     id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
-#     title = models.CharField(max_length=255)
-#     description = models.TextField()
+class KeynoteSpeaker(models.Model):
+    id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
 
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+    speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE)
+    keynote = models.ForeignKey(Keynote, on_delete=models.CASCADE)
 
-#     speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.speaker} - {self.keynote}"
 
 class SpeakerSocialMedia(models.Model):
     id = models.UUIDField(default=uuid4, editable=False, primary_key=True)
